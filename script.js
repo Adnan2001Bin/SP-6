03c12a59efc718716df4849e6ce768661ea1afc2
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-09-08 10:32:30 +0600

    delete

--- script.js
+++ script.js
@@ -53,7 +53,6 @@ closeModal.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
